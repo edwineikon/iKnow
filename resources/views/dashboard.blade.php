ed05316a62ca2f17c98b76019b371651531b9ac2
yogie setiarko <ygdgtl@gmail.com>
2016-11-03 11:26:08 +0700

    dasbor berubah

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,29 @@
-@extends('shared.layout')
+@extends('shared.layoutwithoutnavbar')
 @section('title', 'Dashboard')
 @section('content')
 <div class="container">
-    <h2>Dashboard</h2>
+    <!-- <h2>Dashboard</h2> -->
     <div class="row">
-        <div class="col-md-12">
-            TODO: Show something
+        <div class="col-md-12" style="background-color:white;padding-top:10%;margin-top:4%;">
+			<div class="col-md-3">
+				<img src="public/img/bpjs_logo600x140.png" class="img-responsive" alt="Cinque Terre">
+			</div>
+			<div class="col-md-3">
+			</div>
+			<div class="col-md-3">
+			</div>
+			<div class="col-md-3">
+			</div>			
+        </div>
+        <div class="col-md-12" style="background-color:#9CDD79;padding-top:0.3%;">
+        </div>
+		<div class="col-md-12" style="background-color:#1699E1;padding-top:20%;">
+        </div>
+		<div class="col-md-12" style="background-color:#C6ECF5;padding-top:30%;">
+        </div>
+        <div class="col-md-12" style="background-color:#9CDD79;padding-top:0.2%;">
+        </div>
+		<div class="col-md-12" style="background-color:#1699E1;padding-top:8%;">
         </div>
     </div>
 </div>
